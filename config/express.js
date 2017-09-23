9270e4ea919b7237f0490d0e37a1914df0e4cb68
thwestz <chaiwat.wongchuen@g.swu.ac.th>
2017-09-23 12:10:34 +0700

    connect to mongoDB

--- config/express.js
+++ config/express.js
@@ -3,6 +3,9 @@ import express from 'express';
 import morgan from 'morgan';
 import compression from 'compression';
 import bodyParser from 'body-parser';
+import session from 'express-session';
+import passport from 'passport';
+
 
 module.exports = function () {
     const app = express();
@@ -13,8 +16,21 @@ module.exports = function () {
     else {
         app.use(compression());
     }
-
     app.use(bodyParser.json());
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    var config = require('./config');
+    app.use(session({
+        secret: config.sessionSecret,
+        resave: false,
+        saveUninitializeed: true
+    }));
+    app.use(passport.initialize()); // start passport
+    app.use(passport.session()); // use session via express-session
     require('../app/routes/index.route')(app);
+    require('../app/routes/user.route')(app);
+
     return app;
 }
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // add to file ./index.js
+import mongoose from './config/mongoose';
 import express from './config/express';
 
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.PORT = process.env.PORT || 3000;
 
+var db = mongoose();
 var app = express();
 
 app.listen(process.env.PORT, () => {
